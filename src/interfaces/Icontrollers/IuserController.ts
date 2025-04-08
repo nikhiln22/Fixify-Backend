b077eaca3b1cec91b45dd5fbfd291f3bc8157fd5
nikhiln22 <nikhilnhegde@gmail.com>
2025-04-08 09:34:12 +0530

    some changes

--- src/interfaces/Icontrollers/IuserController.ts
+++ src/interfaces/Icontrollers/IuserController.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from 'express'
 
-
 export interface IuserController {
     register(req: Request, res: Response): Promise<void>
     verifyOtp(req: Request, res: Response): Promise<void>
